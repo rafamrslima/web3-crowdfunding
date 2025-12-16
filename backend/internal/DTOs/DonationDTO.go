d83422a9b2f31fc05594d9865061bfdba04d05cc
Rafael De Moraes Lima <rafaelmrslima@gmail.com>
2025-12-17 00:17:57 +0100

    fixing dto types

--- backend/internal/DTOs/DonationDTO.go
+++ backend/internal/DTOs/DonationDTO.go
@@ -1,16 +1,22 @@
 package dtos
 
+import (
+	"time"
+
+	"github.com/ethereum/go-ethereum/common"
+)
+
 type DonationDTO struct {
 	CampaignId int    `json:"campaignId"`
 	Value      string `json:"value"`
 }
 
 type DonationViewDTO struct {
-	Donor       string `json:"donor"`
-	CampaignId  string `json:"campaignId"`
-	Title       string `json:"title"`
-	Description string `json:"description"`
-	CreatedAt   string `json:"createdAt"`
-	Image       string `json:"image"`
-	Amount      int    `json:"amount"`
+	Donor       common.Address `json:"donor"`
+	CampaignId  string         `json:"campaignId"`
+	Title       string         `json:"title"`
+	Description string         `json:"description"`
+	CreatedAt   time.Time      `json:"createdAt"`
+	Image       string         `json:"image"`
+	Amount      int64          `json:"amount"`
 }
